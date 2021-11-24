76332c1c4c5c197f2ccb2221f9539e1e0e115e60
Ilham <Ilham@DESKTOP-TUALS31>
2021-11-24 10:25:03 +0700

    Perulangan Pertama

---
+++ src/cpm/sqa/perulangan/perulangan.java
@@ -0,0 +1,16 @@
+package cpm.sqa.perulangan;
+
+import java.util.Iterator;
+
+public class perulangan {
+
+	public static void main(String[] args) {
+		int n = 8;
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf("%d x %d = %d%n", n, i, n*i);
+			
+		}
+		
+	}
+
+}
